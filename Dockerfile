16768f568fe31e83ab9702153a193e837c65de91
Florian Dorn <florian.dorn@gmail.com>
2018-03-05 21:31:46 +0100

    WIP: don't use speedtest data, but get it from WAN device via upnp

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,27 @@
-FROM tianon/speedtest
+FROM ubuntu:xenial
 
-RUN apk add --update gnuplot && rm -rf /var/cache/apk/*
+RUN apt-get update
+
+RUN apt-get install -y --no-install-recommends ca-certificates python3 python3-pip gnuplot curl xmlstarlet
+
+RUN apt-get download gupnp-tools libgssdp-1.0-3 libglib2.0-0 libsoup2.4-1 libffi6 libxml2 libsqlite3-0 libicu55
+
+RUN dpkg --force-all -i *.deb
+
+ENV SPEEDTEST_VERSION 1.0.2
+
+RUN pip3 install speedtest-cli==$SPEEDTEST_VERSION
 
 ADD cmd.sh /
+
+ADD *.xml /
 
 ADD speedtest.gnu /
 
 ADD data/ /data
 
 VOLUME /data
+
+ENV GSSDP_INTERFACE="wlp4s0"
 
 CMD ["/cmd.sh"]
--- cmd.sh
+++ cmd.sh
@@ -1,8 +1,44 @@
 #!/bin/sh
 
+timestamp() {
+	date +"%s"
+}
+WAN_CIC_IP=$(gssdp-discover -i $GSSDP_INTERFACE -n ${GSSDP_TINMEOUT:-1} -m available --target="urn:schemas-upnp-org:service:WANCommonInterfaceConfig:1" | sed -ne "s/  Location: \(.*\)/\1/p")
+echo $WAN_CIC_IP
+get_total_bytes_sent() {
+	curl -s "$WAN_CIC_IP" -H "Content-Type: text/xml; charset="utf-8"" -H "SoapAction:urn:schemas-upnp-org:service:WANCommonInterfaceConfig:1#GetTotalBytesSent" -d "@GetTotalBytesSent.xml" | xmlstarlet sel -t -v "//NewTotalBytesSent"
+}
+get_total_bytes_received() {
+	curl -s "$WAN_CIC_IP" -H "Content-Type: text/xml; charset="utf-8"" -H "SoapAction:urn:schemas-upnp-org:service:WANCommonInterfaceConfig:1#GetTotalBytesReceived" -d "@GetTotalBytesReceived.xml" | xmlstarlet sel -t -v "//NewTotalBytesReceived"
+}
+
 test -f /data/speedtest.gnu || cp /speedtest.gnu /data/
 touch /data/speedtest.csv
 
+start=$(timestamp)
+start_sent=$(get_total_bytes_sent)
+start_received=$(get_total_bytes_received)
+
 speedtest-cli --csv | grep "," >> /data/speedtest.csv
+
+end=$(timestamp)
+end_sent=$(get_total_bytes_sent)
+end_received=$(get_total_bytes_received)
+
+echo "duration: "$(($end - $start ))
+echo "sent: "$(( ($end_sent - $start_sent) ))
+echo "received: "$(( ($end_received - $start_received )))
+
+echo "print (($end_sent - $start_sent)/($end - $start)*8)\n" | perl
+echo ""
+echo "print (($end_received - $start_received)/($end - $start)*8)\n" | perl
+
+echo -1,\
+router,\
+Vienna,\
+$(date '+%Y-%m-%dT%H:%M:%S'),\
+0,0,\
+$(echo "print (("$end_sent" - "$start_sent")/("$end" - "$start")*8)\n" | perl),\
+$(echo "print (("$end_received" - "$start_received")/("$end" - "$start")*8)\n" | perl) >> /data/speedtest.csv
 
 gnuplot /data/speedtest.gnu > /data/speedtest.svg
